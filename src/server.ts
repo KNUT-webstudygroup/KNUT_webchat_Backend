0b2f5a0bf6735d7946ba81c789ffae0002c3a664
okxooxoo <okxooxoo@naver.com>
2023-04-07 18:34:31 +0900

    add: login response

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
 // 사실상 깡통
 import express from "express";
+import cors from "cors";
 import { createServer } from "http";
 import { WebSocket } from "ws";
 
 // http 서버 넣어주자.
-const port = 4300
+const port = 4300;
 const app = express();
 app.use(express.json());
-app.get('/',(req,ret)=>{
+app.use(cors());
 
-    ret.json({"key":"val"})
-})
+app.get("/", (req, res) => {
+  res.json({ key: "value" });
+});
+
+app.post("/login", (req, res) => {
+  console.log(
+    `요청에서 온 아이디는 ${req.body.id}, 비밀번호는 ${req.body.pw}입니다.`
+  );
+  res.json({ key: true });
+});
 
 app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
