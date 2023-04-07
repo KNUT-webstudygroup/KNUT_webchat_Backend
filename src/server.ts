e643c1efe89764a9b8e46f6489965bcfe970b43b
Lutica_CANARD <presan100@gmail.com>
2023-04-07 17:34:24 +0900

    feat: server setting

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,16 @@
 import express from "express";
 import { createServer } from "http";
 import { WebSocket } from "ws";
+
+// http 서버 넣어주자.
+const port = 4300
+const app = express();
+app.use(express.json());
+app.get('/',(req,ret)=>{
+
+    ret.json({"key":"val"})
+})
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
