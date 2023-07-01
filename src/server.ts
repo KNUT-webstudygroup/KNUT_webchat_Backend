b8c76bf5a5c2bacec1bdb1bd5493909ea5f1ca13
Lutica_CANARD <presan100@gmail.com>
2023-07-02 00:41:14 +0900

    fix : fix about DB connector

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { userLogin } from "./controller/member/login"
 import { memberRegister } from './controller/member/register';
 import session from 'express-session'
 import IdPwFinderRouter from './controller/member/findIdPw'
+import dotenv from 'dotenv'
+dotenv.config() //...
 
 // http 서버 넣어주자.
 const port = 4300;
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,16 +1,40 @@
 // https://velog.io/@yhg0337/kyselyplanetscaleprisma
 
 import { DB } from '../db/types';
-import { Kysely } from 'kysely'
-import { MysqlDialect } from 'kysely';
-import { createPool } from 'mysql2';
+import { Kysely,MysqlDialect } from 'kysely'
+import { PlanetScaleDialect } from 'kysely-planetscale'
+import { createPool } from 'mysql2' // do not use 'mysql2/promises'!
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const db = new Kysely<DB>({ 
-  // DB는 prisma-kysely를 통해 만들어진 타입입니다.
-  dialect: new MysqlDialect({
-    pool: createPool({
-      database: 'knut-web-dev',
-      host: process.env.DATABASE_URL,
-    })
+const genSecret = () => {
+  if (process.env.DATABASE_URL) {
+    const url = process.env.DATABASE_URL.split('/')[2];
+    const username = url.split(':')[0];
+    const password = url.split(':')[1].split('@')[0];
+    return { username:username, password:password};
+  }
+};
+
+/*export const db = new Kysely<DB>({ // DB는 prisma-kysely를 통해 만들어진 타입입니다.
+  dialect: new PlanetScaleDialect({
+    url:process.env.DATABASE_URL
   }),
-});
+});*/
+const dialect = new MysqlDialect({
+	pool: createPool({
+		database: "knut-web-dev",
+		host: "gcp.connect.psdb.cloud",
+		user: genSecret()?.username||'',
+		password: genSecret()?.password||'',
+		port: 3306,
+    ssl:{"rejectUnauthorized":true} // 맘에는 안드는데 일단 해결책을 찾기전까지..
+	})
+})
+
+
+
+
+export const db = new Kysely<DB>({
+	dialect
+})
