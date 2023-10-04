3ea761773ae93531939fb78b72dcb24f38ca24fa
Lutica_CANARD <presan100@gmail.com>
2023-10-04 22:57:24 +0900

    merge with front by express-vite

---
+++ src/front/App.tsx
@@ -0,0 +1,40 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+import React, { Component, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './login'
+import Register from './register'
+import MainPage from './mainapp/main';
+import SettingPage from './Setting/setting';
+import {
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+  } from 'recoil';
+
+
+
+function App() {
+	const [count, setCount] = useState(0)
+	const fronturi = ''
+	return (
+		<div className='App'>
+			<RecoilRoot>
+				<BrowserRouter>
+					<Routes>
+						<Route path={fronturi + "/login"} element={<Login />}></Route>
+						<Route path={fronturi + "/regist/"} element={<Register />}></Route>
+						<Route path={fronturi + "/"}element={<MainPage />}></Route>
+						<Route path={fronturi + "/setting/"}element={<SettingPage/>}></Route>
+						<Route path="*" element={<MainPage />}></Route>
+					</Routes>
+				</BrowserRouter>
+			</RecoilRoot>
+		</div>
+	)
+}
+
+export default App
---
+++ src/server/db/types.ts
@@ -0,0 +1,68 @@
+import type {ColumnType} from "kysely";
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+	? ColumnType<S, I | undefined, U>
+	: ColumnType<T, T | undefined, T>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export type AccountFindQuest = {
+	userId: number;
+	loginId: string;
+	email: string;
+	questindex: number;
+	quest: string;
+	answer: string;
+};
+export type Group = {
+	id: Generated<number>;
+	name: string;
+	desc: string;
+	admin: number;
+	capacity: number;
+	total: number;
+	createdAt: Generated<Timestamp>;
+};
+export type GroupChatsImage = {
+	imageLinkId: number;
+	count: number;
+	link: string;
+};
+export type GroupProfile = {
+	id: Generated<number>;
+	name: string;
+	desc: string;
+};
+export type GroupTags = {
+	groupId: number;
+	groupTag: string;
+};
+export type Message = {
+	id: Generated<number>;
+	content: string;
+	sender: number;
+	groupId: number;
+	imageLinkId: number | null;
+	sendTime: Generated<Timestamp>;
+};
+export type User = {
+	id: Generated<number>;
+	loginId: string;
+	pw: string;
+	email: string;
+	phone: string | null;
+	createdAt: Generated<Timestamp>;
+	updatedAt: Generated<Timestamp>;
+};
+export type UserGroup = {
+	userid: number;
+	groupId: number;
+};
+export type DB = {
+	AccountFindQuest: AccountFindQuest;
+	GROUPS: Group;
+	GroupChatsImage: GroupChatsImage;
+	GroupProfile: GroupProfile;
+	GroupTags: GroupTags;
+	MESSAGES: Message;
+	USERS: User;
+	UserGroup: UserGroup;
+};
